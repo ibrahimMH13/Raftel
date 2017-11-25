b0221d321f7e3eb78d7b892614808d94d7be12f4
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-11-25 19:22:59 +0200

    [006] enable auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
             <div class="content">
                 <div class="title m-b-md">
                         <h1>
-                         RafteL
+                         Raftel
                         </h1>
                 </div>
 
