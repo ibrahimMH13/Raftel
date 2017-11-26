7e341a1f3009666bb0b834bd7ec1c187d76f69e6
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-11-26 19:25:25 +0200

    [010] define relation between reply and thread

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+
+
+    //relationShip
+
+
+    public function thread(){
+
+        return $this->belongsTo(Reply::class);
+    }
 }
