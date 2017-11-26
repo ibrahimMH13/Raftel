63119dc29b571cb12e2b622dd7da63f2a252a476
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-11-26 21:17:49 +0200

    [020] create helper file and make auth for thread Controller

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -12,6 +12,12 @@ class ThreadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index','show');
+    }
+
     public function index()
     {
         //
