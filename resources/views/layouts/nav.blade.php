b6667a243b552f63f8cd4118f3ab998429f5a38b
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-12-01 18:20:56 +0200

    [052] create new file for nav

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,96 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('APP_NAME', 'Raftel') }}
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                <li><a href="/threads/create">New</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                        Threads <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="/threads">Threads</a>
+                        </li><li>
+                            <a href="/threads?popular">Popular Threads</a>
+                        </li>
+                        @if(auth()->check())
+                            <li>
+                                <a href="{{ '/threads?by='.auth()->user()->name }}">My Threads</a>
+                            </li>
+                        @endif
+                    </ul>
+                </li>
+                @if(\App\Models\Channel::all()->isNotEmpty())
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                            Channels <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            @foreach($channels as $ch)
+                                <li>
+                                    <a href="/threads/{{ $ch->slug }}">{{$ch->name}}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+
+                    </li>
+                @endif
+
+                @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('profile',auth()->user() ) }}">
+                                    My Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                    @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
